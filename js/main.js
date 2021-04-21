5e2be70fcb78dd40b08da9886ec2c3b69eaef44c
Alex-end <80333289+Alex-end@users.noreply.github.com>
2021-04-21 21:56:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -81,3 +81,18 @@ function initMap() {
     map: map,
   });
 }
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
+const smoothLinks = document.querySelectorAll('a[href^="#i"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });}
